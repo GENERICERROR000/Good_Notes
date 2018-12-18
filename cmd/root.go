d2b2f303d21547b6680caa2f450dcd3cebc82eb2
Noah K <nkernis@gmail.com>
2018-12-17 22:49:40 -0500

    error handle for condif

--- cmd/root.go
+++ cmd/root.go
@@ -80,9 +80,13 @@ func initConfig() {
 	viper.AutomaticEnv()
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	} else {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
+
 	fmt.Println("Using config file:", viper.ConfigFileUsed())
 }
 
